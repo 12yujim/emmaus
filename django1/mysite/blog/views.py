a5784b69a7ede00427528fa888f1378d062d936b
Jim Yu <jim.yu343@gmail.com>
2016-02-20 11:00:59 -0500

    updated

--- django1/mysite/blog/views.py
+++ django1/mysite/blog/views.py
@@ -8,29 +8,33 @@ from django.core.mail import send_mail
 
 def index(request):
     return render_to_response('index.html', {
+        'nbar': 'home',
         'posts': Blog.objects.all()[:5]
     })
 
 def about(request):
-    return render_to_response('about.html', {})
+    return render_to_response('about.html', {'nbar': 'about'})
 
 def ministries(request):
     return render_to_response('ministries.html', {
+        'nbar': 'ministries',
         'cores': Core.objects.all(),
         'servants': Servant.objects.all(),
         'praises': Praise.objects.all()
     })
 
 def beliefs(request):
-    return render_to_response('beliefs.html', {})
+    return render_to_response('beliefs.html', {'nbar': 'beliefs'})
 
 def sermon(request):
     return render_to_response('sermon.html', {
+        'nbar': 'sermon',
         'sermons': Sermon.objects.all()
     })
 
 def events(request):
     return render_to_response('events.html', {
+        'nbar': 'events',
         'posts': Blog.objects.all()
     })
 
@@ -51,8 +55,10 @@ def contact(request):
                 ['jly29@cornell.edu'],
             )
             return HttpResponse('Thank you! We\'ll get back to you as soon as possible.')
-    return render(request, 'contact.html',
-        {'errors': errors})
+    return render(request, 'contact.html', {
+        'errors': errors,
+        'nbar': 'contact'
+    })
 
 def view_post(request, slug):   
     return render_to_response('view_post.html', {
